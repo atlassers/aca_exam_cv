f753484d807335f6fe0bd988c5ba8d2efc386a5d
LAPTOP-0SH9ABJ8\veron <veronica.cirillo96@gmail.com>
2021-10-29 16:30:26 +0200

    no message

--- cinema/src/main/java/it/euris/exam/cinema/service/SalaCinematograficaService.java
+++ cinema/src/main/java/it/euris/exam/cinema/service/SalaCinematograficaService.java
@@ -1,5 +1,14 @@
 package it.euris.exam.cinema.service;
 
+import java.util.List;
+import it.euris.exam.cinema.data.dto.SalaCinematograficaDto;
 public interface SalaCinematograficaService {
 
+  public SalaCinematograficaDto add(SalaCinematograficaDto salaCinematograficaDto);
+  
+  public List<SalaCinematograficaDto> getAll();
+
+  public SalaCinematograficaDto get(Long id);
+  
+  public SalaCinematograficaDto getIncasso(Long id);
 }
--- cinema/src/main/java/it/euris/exam/cinema/service/impl/SalaCinematograficaServiceImpl.java
+++ cinema/src/main/java/it/euris/exam/cinema/service/impl/SalaCinematograficaServiceImpl.java
@@ -1,7 +1,12 @@
 package it.euris.exam.cinema.service.impl;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import it.euris.academy.cloud.data.model.Nation;
+import it.euris.exam.cinema.data.dto.SalaCinematograficaDto;
+import it.euris.exam.cinema.data.model.SalaCinematografica;
 import it.euris.exam.cinema.repository.SalaCinematograficaRepository;
 import it.euris.exam.cinema.service.SalaCinematograficaService;
 
@@ -9,5 +14,34 @@ import it.euris.exam.cinema.service.SalaCinematograficaService;
 public class SalaCinematograficaServiceImpl implements SalaCinematograficaService{
  @Autowired
  SalaCinematograficaRepository salaCinematograficaRepository;
+
+@Override
+public SalaCinematograficaDto add(SalaCinematograficaDto salaCinematograficaDto) {
+  return null;
+}
+
+@Override
+public List<SalaCinematograficaDto> getAll() {
+  // TODO Auto-generated method stub
+  return null;
+}
+
+@Override
+public SalaCinematograficaDto get(Long id) {
+  // TODO Auto-generated method stub
+  return null;
+}
+
+@Override
+public SalaCinematograficaDto getIncasso(Long id) {
+  Optional<SalaCinematografica> sala = salaCinematograficaRepository.findById(id);
+  if(sala.isPresent()) {
+      return sala.get().toDto().;
+  }
+  return null;
+}
+}
  
+
+//aggiungere metodo in dto per verificare posto in sala.
 }
